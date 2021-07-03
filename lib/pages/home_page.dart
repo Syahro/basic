a0d2ce1f0c818a8023439b4fe83a41f2e218d81c
Arief Syahroni <riefsyah31@gmail.com>
2021-07-03 23:00:23 +0700

    modified homePage and add new color on theme

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:basic/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,88 +8,114 @@ class HomePage extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 35,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Image.asset(
-                'assets/images/primary.png',
-                width: 140,
-                height: 140,
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Anne Margaritha',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'UX Designer',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: greyColor,
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'assets/images/item1.png',
-                    width: 80,
-                    height: 80,
+                Image.asset(
+                  'assets/images/primary.png',
+                  width: 140,
+                  height: 140,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Anne Margaritha',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Image.asset(
-                    'assets/images/item2.png',
-                    width: 80,
-                    height: 80,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'UX Designer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: greyColor,
                   ),
-                  Image.asset(
-                    'assets/images/item3.png',
-                    width: 80,
-                    height: 80,
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 30,
+                  children: [
+                    Image.asset(
+                      'assets/images/item1.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item2.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item3.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item4.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item5.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item6.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: whiteColor,
                   ),
-                  Image.asset(
-                    'assets/images/item4.png',
-                    width: 80,
-                    height: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item5.png',
-                    width: 80,
-                    height: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item6.png',
-                    width: 80,
-                    height: 80,
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 76,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,3 +4,4 @@ Color primaryColor = Color(0XFF314728);
 Color greyColor = Color(0XFFA9B0A6);
 Color orangeColor = Color(0XFFF9B650);
 Color backgroundColor = Color(0XFFE5E5E5);
+Color whiteColor = Color(0XFFFFFFFF);
